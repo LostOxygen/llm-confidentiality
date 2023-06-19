7f13c1c60ed232fd8e0117616325da89b6792c72
LostOxygen <jonathan.evertz@gmail.com>
2023-06-19 18:44:59 +0200

    added abstract LLM class

--- src/llm.py
+++ src/llm.py
@@ -1,1 +1,19 @@
 """library for LLM models, functions and helper stuff"""
+
+from abc import ABC, abstractmethod
+
+class LLM(ABC):
+    """abstract implementation of a genereric LLM model"""
+
+    @abstractmethod
+    def predict(self, text: str) -> str:
+        """
+        predicts a response for a given text input
+
+        Parameters:
+            text: str - the text to predict a response for
+
+        Returns:
+            response: str - the response
+        """
+        pass

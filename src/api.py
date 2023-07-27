4d7956473fa064d9e5b9c72a363f7583f0b3cce9
LostOxygen <jonathan.evertz@gmail.com>
2023-07-27 09:08:30 +0200

    improved api for chat inputs

--- src/api.py
+++ src/api.py
@@ -1,17 +1,17 @@
 """API class implementation to interact with the ChatVisuzalization class"""
 from datetime import datetime
-from typing import Final
+from typing import Final, List
 import os
 
 PATH: Final[str] = "/tmp/llm_chat/"
 
 class ChatAPI:
-    """socket API class to interact with ChatVisualization class"""
+    """API class to interact with the ChatVisualization class via temp files"""
     def __init__(self):
         pass
 
     @staticmethod
-    def add_message(user: str, message: str):
+    def add_message(user: str, message: str) -> None:
         """adds messages to the chat visualization"""
         # check if the paths are valid
         if not os.path.exists(PATH):
@@ -23,3 +23,36 @@ class ChatAPI:
 
         with open(PATH+"chat_log.txt", "a", encoding="utf-8") as chat_file:
             chat_file.write(final_msg)
+
+
+    @staticmethod
+    def add_input_message(message: str) -> None:
+        """adds messages to the chat visualization"""
+        # check if the paths are valid
+        if not os.path.exists(PATH):
+            os.makedirs(PATH)
+
+        with open(PATH+"chat_input_log.txt", "a", encoding="utf-8") as chat_file:
+            chat_file.write(message.encode("utf-8"))
+
+
+    @staticmethod
+    def get_first_message(file_name: str) -> str:
+        """returns the first (oldest) message in the input chat log and removes it from the log"""
+        with open(PATH+file_name, "r", encoding="utf-8") as chat_file:
+            complete_log = chat_file.readlines()
+            first_line = complete_log.pop(0) # remove the first line from the log
+
+        with open(PATH+"chat_log.txt", "w", encoding="utf-8") as chat_file:
+            chat_file.write(complete_log)
+
+        return first_line
+
+
+    @staticmethod
+    def get_all_messages(file_name: str) -> List[str]:
+        """returns all messages in the input chat log and clears the log"""
+        with open(PATH+file_name, "r", encoding="utf-8") as chat_file:
+            complete_log = chat_file.readlines()
+
+        return complete_log

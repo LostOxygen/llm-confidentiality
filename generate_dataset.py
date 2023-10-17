0a7df53c8cf8f6ea94392ec69ef6b378c6287d2e
jonathan <jonathan.evertz@gmail.com>
2023-10-17 14:47:54 +0200

    removed name_suffix

--- generate_dataset.py
+++ generate_dataset.py
@@ -99,10 +99,6 @@ def main(
         if "all" in attacks:
             attacks = ATTACK_LIST
 
-    # add '-' in front of the name suffix
-    if name_suffix != "" and not name_suffix.startswith("-"):
-        name_suffix = "-" + name_suffix
-
     print("\n"+"#"*os.get_terminal_size().columns)
     print(f"## {TColors.OKBLUE}{TColors.BOLD}Date{TColors.ENDC}: " + \
           str(datetime.datetime.now().strftime("%A, %d. %B %Y %I:%M%p")))

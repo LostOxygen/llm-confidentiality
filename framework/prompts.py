f665b60bb15703a8283e73965de05fabd5247eb4
LostOxygen <jonathan.evertz@gmail.com>
2023-11-09 11:02:49 +0100

    added EOS token logs processor

--- framework/prompts.py
+++ framework/prompts.py
@@ -4,7 +4,7 @@ from typing import Final, List, Type, Dict
 
 import torch
 
-from transformers import StoppingCriteria, AutoTokenizer
+from transformers import StoppingCriteria, AutoTokenizer, LogitsProcessor
 
 SECRET_KEY: Final[str] = "1337"
 
@@ -406,3 +406,31 @@ class AttackStopping(StoppingCriteria):
             if stop in decoded_input_ids:
                 return True
         return False
+
+
+class EosTokenRewardLogitsProcessor(LogitsProcessor):
+    """
+    Logits processor that increased the probability of the eos_token with the length of the input.
+    At 80% of the max length the eos_token get's more and more likely to be generated.
+    """
+    def __init__(self,  eos_token_id: int, max_length: int):
+        super().__init__()
+        if not isinstance(eos_token_id, int) or eos_token_id < 0:
+            raise ValueError(f"`eos_token_id` has to be a positive integer, but is {eos_token_id}")
+
+        if not isinstance(max_length, int) or max_length < 1:
+            raise ValueError(f"`max_length` has to be a integer bigger than 1, but is {max_length}")
+
+        self.eos_token_id = eos_token_id
+        self.max_length=max_length
+
+    def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor) -> torch.FloatTensor:
+        cur_len = input_ids.shape[-1]
+        # start to increese the reward of the eos_token from 80% max length progressively on length
+        for cur_len in (max(0,int(self.max_length*0.8)), self.max_length ):
+            ratio = cur_len/self.max_length
+            num_tokens = scores.shape[1] # size of vocab
+            scores[:, [i for i in range(num_tokens) if i != self.eos_token_id]] =\
+            scores[:, [i for i in range(num_tokens) if i != self.eos_token_id]]*ratio*10*torch.exp(-torch.sign(scores[:, [i for i in range(num_tokens) if i != self.eos_token_id]]))
+            scores[:, self.eos_token_id] = 1e2*ratio
+        return scores

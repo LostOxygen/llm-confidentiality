c834493264f4e68296a3c150884d0f8f025b61ed
jonathan <jonathan.evertz@gmail.com>
2023-11-26 12:58:52 +0100

    added yelp finetune

---
+++ various_scripts/yelp_finetune.py
@@ -0,0 +1,113 @@
+"""test script to finetune llama2 models on the yelp dataset"""
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python3
+
+import os
+import datetime
+import socket
+from typing import (
+    Tuple,
+)
+
+import numpy as np
+import torch
+from transformers import (
+    AutoTokenizer,
+    AutoModelForSequenceClassification,
+    BitsAndBytesConfig,
+    TrainingArguments,
+    Trainer,
+)
+from datasets import load_dataset
+import evaluate
+
+from colors import TColors
+
+LLM_TYPE = "llama2-7b"
+OUTPUT_NAME= "llama2-7b-yelp"
+
+
+def compute_metrics(eval_pred: Tuple[np.ndarray, np.ndarray]) -> dict:
+    logits, labels = eval_pred
+    predictions = np.argmax(logits, axis=-1)
+    return metric.compute(predictions=predictions, references=labels)
+
+
+def tokenize_function(data: str) -> str:
+    return tokenizer(data["text"], padding="max_length", truncation=True)
+
+
+# setting devies and variables correctly
+if not torch.cuda.is_available():
+    device = "cpu"
+else:
+    device = "cuda:0"
+
+# print system information
+print("\n"+"#"*os.get_terminal_size().columns)
+print(f"## {TColors.OKBLUE}{TColors.BOLD}Date{TColors.ENDC}: " + \
+        str(datetime.datetime.now().strftime("%A, %d. %B %Y %I:%M%p")))
+print(f"## {TColors.OKBLUE}{TColors.BOLD}System{TColors.ENDC}: " \
+        f"{torch.get_num_threads()} CPU cores with {os.cpu_count()} threads and " \
+        f"{torch.cuda.device_count()} GPUs on {socket.gethostname()}")
+print(f"## {TColors.OKBLUE}{TColors.BOLD}Device{TColors.ENDC}: {device}")
+if torch.cuda.is_available():
+    print(f"## {TColors.OKBLUE}{TColors.BOLD}GPU Memory{TColors.ENDC}: " \
+            f"{torch.cuda.mem_get_info()[1] // 1024**2} MB")
+print(f"## {TColors.OKBLUE}{TColors.BOLD}LLM{TColors.ENDC}: {LLM_TYPE}")
+print(f"## {TColors.OKBLUE}{TColors.BOLD}Output Name{TColors.ENDC}: {OUTPUT_NAME}")
+
+
+tokenizer = AutoTokenizer.from_pretrained(
+        LLM_TYPE,
+        use_fast=False
+    )
+tokenizer.pad_token = tokenizer.eos_token
+
+config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_compute_dtype=torch.float16
+)
+
+model = AutoModelForSequenceClassification.from_pretrained(
+        LLM_TYPE,
+        device_map="auto",
+        quantization_config=config,
+        low_cpu_mem_usage=True,
+        trust_remote_code=True,
+        cache_dir=os.environ["TRANSFORMERS_CACHE"],
+        num_labels=5
+    )
+
+# disable caching for finetuning
+model.config.use_cache = False
+model.config.pretraining_tp = 1
+metric = evaluate.load_metric("accuracy")
+
+dataset = load_dataset("yelp_review_full")
+tokenized_dataset = dataset.map(tokenize_function, batched=True)
+
+training_args = TrainingArguments(
+    output_dir="test_trainer",
+    evaluation_strategy="epoch"
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_dataset["train"].shuffle(seed=42),
+    eval_dataset=tokenized_dataset["test"].shuffle(seed=42),
+    compute_metrics=compute_metrics,
+)
+
+trainer.train()
+trainer.model.save_pretrained(
+        os.path.join("", OUTPUT_NAME),
+        safe_serialization=True,
+        save_adapter=True,
+        save_config=True
+    )
+trainer.tokenizer.save_pretrained(os.path.join("", OUTPUT_NAME))
+

5eafb628ff2a1955786e9c1158768b42087531e0
LostOxygen <jonathan.evertz@gmail.com>
2024-02-22 08:59:50 +0100

    debug print

--- framework/llm.py
+++ framework/llm.py
@@ -507,10 +507,10 @@ class LLM():
                 # so only the models' actual response remains
                 history = "<start_of_turn>"+response[0]+"<end_of_turn>"
                 print("formatted_messages", formatted_messages)
-                print("response", response)
                 response = response[0].replace(
                     formatted_messages.replace("<start_of_turn>", "").replace("<end_of_turn>", "")
                 , "", 1)
+                print("response", response)
 
             case ("gpt-3.5" | "gpt-3.5-turbo" | "gpt-4" | "gpt-4-turbo"):
                 messages = [

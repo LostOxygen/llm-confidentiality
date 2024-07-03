d5f3a222e783e8f5001a1dba5a624a6d9600a5a3
LostOxygen <jonathan.evertz@gmail.com>
2024-07-03 15:45:33 +0200

    kinda fixed llama3 gmail tool usage

--- various_scripts/llm_mail_test.py
+++ various_scripts/llm_mail_test.py
@@ -1,7 +1,8 @@
 """langchain llm email test script"""
 import os
 
-from langchain.agents import AgentExecutor, create_structured_chat_agent
+from langchain import hub
+from langchain.agents import AgentExecutor, create_structured_chat_agent, create_openai_functions_agent
 from langchain_experimental.llms.ollama_functions import OllamaFunctions
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.agent_toolkits import GmailToolkit
@@ -9,6 +10,9 @@ from langchain_community.tools.gmail.utils import (
     build_resource_service,
     get_gmail_credentials,
 )
+from langchain_openai import ChatOpenAI
+from langchain_core.utils.function_calling import convert_to_openai_function
+
 from colors import TColors
 
 def main() -> None:
@@ -45,14 +49,53 @@ def main() -> None:
     api_resource = build_resource_service(credentials=credentials)
     toolkit = GmailToolkit(api_resource=api_resource)
 
-    model = OllamaFunctions(model="llama3", temperature=0, format="json")
-    # model.bind_tools(toolkit.get_tools())
+    model = OllamaFunctions(model="llama3", temperature=0)
+    # model = ChatOpenAI(temperature=0)
     # Create prompts for the agents
     system_prompt = """
-    You are a helpful AI assistant! You are allowed to read, write and work 
-    with my personal emails. To do so, you can use the following tools:
-    
-    {tools} with the following names: {tool_names}
+        Respond to the human as helpfully and accurately as possible. You have access to the following tools:
+
+        {tools}
+
+        Use a json blob to specify a tool by providing an action key (tool name) and an action_input key (tool input).
+        Make sure to use the tool input correctly with its specified formats and types. For example:
+        $INPUT: {{
+            "message": "text",
+            "subject": "subject text"
+            "to": ["person1", "person2", ...]
+        }}
+        Only append a single string to the message key. Not a dictionary or anything else.
+
+        Valid "action" values: "Final Answer" or {tool_names}
+
+        Provide only ONE action per $JSON_BLOB, as shown:
+
+        ```
+        {{
+        "action": $TOOL_NAME,
+        "action_input": $INPUT
+        }}
+        ```
+
+        Follow this format:
+
+        Question: input question to answer
+        Thought: consider previous and subsequent steps
+        Action:
+        ```
+        $JSON_BLOB
+        ```
+        Observation: action result
+        ... (repeat Thought/Action/Observation N times)
+        Thought: I know what to respond
+        Action:
+        ```
+        {{
+        "action": "Final Answer",
+        "action_input": "Final response to human"
+        }}
+
+        Begin! Reminder to ALWAYS respond with a valid json blob of a single action. Use tools if necessary. Respond directly if appropriate. Format is Action:```$JSON_BLOB```then Observation
     """
     prompt = ChatPromptTemplate.from_messages(
         [
@@ -60,12 +103,20 @@ def main() -> None:
             ("human", "{input}\n {agent_scratchpad}"),
         ]
     )
+    model.bind_tools(
+        [convert_to_openai_function(t) for t in toolkit.get_tools()], 
+        tool_choice="any",
+    )
 
     agent = create_structured_chat_agent(
         tools=toolkit.get_tools(),
         llm=model,
         prompt=prompt,
     )
+    # instructions = """You are an assistant."""
+    # base_prompt = hub.pull("langchain-ai/openai-functions-template")
+    # prompt = base_prompt.partial(instructions=instructions)
+    # agent = create_openai_functions_agent(model, toolkit.get_tools(), prompt)
 
     agent_executor = AgentExecutor(
         agent=agent,
@@ -78,7 +129,8 @@ def main() -> None:
 
     agent_executor.invoke(
         {
-            "input": "What is the title of my latest email?"
+            "input": "Create a gmail draft for me with the subject 'Hello'" \
+            " and the message 'World'. It should be sent to ''.",
         }
     )
 

b6240362c281ef384b685db32f734442bc091202
LostOxygen <jonathan.evertz@gmail.com>
2024-07-04 13:56:21 +0200

    added google drive test implementations

---
+++ various_scripts/gdrive_test.py
@@ -0,0 +1,18 @@
+"""test script to test the google drive search tool"""
+import os
+from langchain_googledrive.utilities.google_drive import GoogleDriveAPIWrapper
+from langchain_googledrive.tools.google_drive.tool import GoogleDriveSearchTool
+
+os.environ["GOOGLE_ACCOUNT_FILE"] = "google_credentials.json"
+
+# By default, search only in the filename.
+tool = GoogleDriveSearchTool(
+    api_wrapper=GoogleDriveAPIWrapper(
+        folder_id="12QFKRqhM_R6-MMryXuOMIMGzIqaYGupt",
+        num_results=1,
+        template="gdrive-query-in-folder",  # Search in the body of documents
+        mode="documents",
+    )
+)
+
+print(tool.run("soup"))

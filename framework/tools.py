a32bc48b8999635e0a0cd8d0aa033c85a8a01d7f
LostOxygen <jonathan.evertz@gmail.com>
2024-07-16 16:05:17 +0200

    added new tool implementation

--- framework/tools.py
+++ framework/tools.py
@@ -1,9 +1,176 @@
 """library for langchain tools"""
-from typing import Callable
+from typing import Callable, Type, Union
 
 from langchain.tools import BaseTool
+from langchain_core.pydantic_v1 import BaseModel, Field
+
+from .tool_data import CLOUD_DB, MAIL_INBOX, NOTES, CALENDAR_EVENTS
+
+# currently used new tools
+class GetCloudFilesSchema(BaseModel):
+    """Input for GetCloudFilesTool."""
+
+    file_name: str = Field(
+        ...,
+        description="The name of the file to search for.",
+    )
+
+class GetCloudFilesTool(BaseTool):
+    """Tool that returns files from a cloud database"""
+
+    name: str = "get_cloud_files"
+    description: str = (
+        "Use this tool to search and receive files from a cloud database. "
+        "Use file_name:file_name to specify which file should be returned "
+        "or use file_name:all to return all files."
+    )
+    args_schema: Type[GetCloudFilesSchema] = GetCloudFilesSchema
+
+    def _run(self, file_name: str) -> Union[dict, str]:
+        """Use the tool."""
+        if file_name == "all":
+            return CLOUD_DB
+
+        result = CLOUD_DB.get(file_name, "File not found in cloud!")
+        return str(result)
 
 
+class GetMailsSchema(BaseModel):
+    """Input for GetMailsTool"""
+
+    query: str = Field(
+        ...,
+        description="The mail search query."
+    )
+
+class GetMailsTool(BaseTool):
+    """Tool that returns emails based on the used query"""
+
+    name: str = "get_mails"
+    args_schema: Type[GetMailsSchema] = GetMailsSchema
+    description: str = (
+        "Use this tool to search for emails with certain senders, subjects, or statuses. "
+        "Example filters include from:sender, subject:subject, is:unread, is:read, exact phrase."
+    )
+
+    def _run(self, query: str) -> Union[dict, list, str]:
+        """Use the tool."""
+        results = []
+
+        for mail in MAIL_INBOX:
+            query_type = query.split(":")[0].strip().lower()
+            query_content = query.split(":")[-1].strip().lower()
+
+            match query_type:
+                case "from":
+                    if mail["from"].lower() == query_content:
+                        results.append(mail)
+
+                case "subject":
+                    if mail["subject"].lower() == query_content:
+                        results.append(mail)
+
+                case "is":
+                    if mail["is"] == query_content:
+                        results.append(mail)
+
+                case _:
+                    return MAIL_INBOX
+
+        if not results:
+            return "No mails found with that query."
+        return results
+
+
+class GetNotesSchema(BaseModel):
+    """Input for GetNotesTool"""
+
+    query: str = Field(
+        ...,
+        description = "The notes query to search for subjects or contents.",
+    )
+
+class GetNotesTool(BaseTool):
+    """Tool that returns notes based on the used query"""
+
+    name: str = "get_notes"
+    args_schema: Type[GetNotesSchema] = GetNotesSchema
+    description: str = (
+        "Use this tool to search for notes with certain subjects or contents. Use the query "
+        "as subject:subject or receive all notes to search for content"
+    )
+
+    def _run(self, query: str) -> Union[list, dict, str]:
+        """Use the tool."""
+
+        results = []
+
+        for note in NOTES:
+            query_type = query.split(":")[0].strip().lower()
+            query_content = query.split(":")[-1].strip().lower()
+
+            match query_type:
+                case "subject":
+                    if note["subject"].lower() == query_content:
+                        results.append(note)
+
+                case _:
+                    return NOTES
+
+        if not results:
+            return "No notes found with that query."
+        return results
+
+
+class GetCalendarEventSchema(BaseModel):
+    """Input for GetNotesTool"""
+
+    query: str = Field(
+        ...,
+        description = "The calendar event query to search for specific events.",
+    )
+
+class GetCalendarEventsTool(BaseTool):
+    """Tool that returns calendar events based on the used query"""
+
+    name: str = "get_calendar_events"
+    args_schema: Type[GetCalendarEventSchema] = GetCalendarEventSchema
+    description: str = (
+        "Use this tool to search for calendar events with certain topics, dates, or locations. "
+        "Use the query as date:date, topic:topic, or location:location."
+    )
+
+    def _run(self, query: str) -> Union[list, dict, str]:
+        """Use the tool."""
+
+        results = []
+
+        for event in CALENDAR_EVENTS:
+            query_type = query.split(":")[0].strip().lower()
+            query_content = query.split(":")[-1].strip().lower()
+
+            match query_type:
+                case "topic":
+                    if event["topic"].lower() == query_content:
+                        results.append(event)
+
+                case "date":
+                    if event["date"].lower() == query_content:
+                        results.append(event)
+
+                case "location":
+                    if event["location"].lower() == query_content:
+                        results.append(event)
+
+                case _:
+                    return CALENDAR_EVENTS
+
+        if not results:
+            return "No notes found with that query."
+        return results
+
+
+# old now mostly unused tools
 class DatabaseTool(BaseTool):
     """
     Custom tool for langchain.
